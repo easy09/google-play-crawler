803a769f771b92973592f45104a1d6c16243bdab
will <will@cn09.com>
2018-09-25 10:56:42 +0800

    取数据完成

--- crawl_play_store.py
+++ crawl_play_store.py
@@ -4,33 +4,48 @@ import sys, io
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.proxy import *
-
-# @author Ranjeet Singh <ranjeetsingh867@gmail.com>
-# Modify it according to your requirements
-
 no_of_reviews = 1000
 
 non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
 # driver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
-driver = webdriver.Firefox(executable_path='c:/soft/geckodriver.exe')
-wait = WebDriverWait(driver, 10)
+# driver = webdriver.Firefox(executable_path='d:/tools/geckodriver.exe')
+# driver = webdriver.Firefox(executable_path='c:/soft/geckodriver.exe')
 
-# class hash
-class_name = {
-    'id-app-title': 'AHFaub'
-}
+# dcap = dict(DesiredCapabilities.PHANTOMJS)
+# dcap["phantomjs.page.settings.userAgent"] = (
+#     "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0")
+# driver = webdriver.PhantomJS("d:/tools/phantomjs.exe", desired_capabilities=dcap)
+
+options = webdriver.FirefoxOptions()
+options.add_argument('-headless')
+driver = webdriver.Firefox(options=options)
+
+wait = WebDriverWait(driver, 10)
 
 # Append your app store urls here
 urls = [
-    "https://play.google.com/store/apps/details?id=com.flipkart.android&hl=en&showAllReviews=true",
+    # "https://play.google.com/store/apps/details?id=com.flipkart.android&hl=en&showAllReviews=true",
+    "https://play.google.com/store/apps/details?id=com.twitter.android&showAllReviews=true"
     # "https://play.google.com/store/apps/details?id=com.amazon.mShop.android.shopping"
     # "http://m.toutiao.com"
 ]
 
 result = []
-i = 0
 for url in urls:
     driver.get(url)
+    time.sleep(2)
+    driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+    time.sleep(2)
+    driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+    time.sleep(2)
+    driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+    time.sleep(2)
+    driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+    time.sleep(2)
+    driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+    time.sleep(2)
+    driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+
     page = driver.page_source
 
     soup_expatistan = BeautifulSoup(page, "html.parser")
@@ -38,69 +53,32 @@ for url in urls:
     # expand_li = expand_div.find_all("li")
     # for li in expand_li:
     #     print(li.text)
-
     expatistan_table = soup_expatistan.find("h1", class_='AHFaub')
-    result[i]['app_name'] = expatistan_table.text
-    result[i]['reviews'] = []
-    j = 0
+    result.append({"title": expatistan_table.text, "reviews": []})
 
     expatistan_table = soup_expatistan.find("div", class_='W4P4ne')
     expatistan_div = expatistan_table.find_all("div", class_='zc7KVe')
 
+    index = len(result) - 1
+
     for ele in expatistan_div:
         user_name = ele.find("span", class_="X43Kjb")
-        result[i]['reviews'][j]['user_name'] = user_name.text
+        star_num = ele.find("div", class_="pf5lIe").find_next()['aria-label']
+        review_div = ele.find("div", class_="UD7Dzf")
+        try:
+            review_div.find_all("span")
+        except AttributeError:
+            review = review_div
+        else:
+            review = review_div.find_all("span")[0].text
 
-    print(result)
-
-    #
-    # expatistan_table = soup_expatistan.find("meta", itemprop="ratingValue")
-    #
-    # print("Rating Value: ", expatistan_table['content'])
-    #
-    # expatistan_table = soup_expatistan.find("meta", itemprop="reviewCount")
-    #
-    # print("Rating Count: ", expatistan_table['content'])
-    #
-    # expatistan_table = soup_expatistan.find("span", class_="reviews-num")
-    #
-    # print("Reviews Count: ", expatistan_table.string)
-    #
-    # soup_histogram = soup_expatistan.find("div", class_="rating-histogram")
-    #
-    # rating_bars = soup_histogram.find_all('div', class_="rating-bar-container")
-    #
-    # for rating_bar in rating_bars:
-    #     print("Rating: ", rating_bar.find("span").text)
-    #     print("Rating count: ", rating_bar.find("span", class_="bar-number").string)
-    #
-    # next_button = driver.find_element_by_xpath(
-    #     '//*[@id="body-content"]/div/div/div[1]/div[2]/div[2]/div[1]/div[4]/button[2]')
-    #
-    # for i in range(0, no_of_reviews):
-    #     try:
-    #         next_button.click()
-    #     except Exception:
-    #         time.sleep(5)
-    #
-    # reviews_div = driver.find_element_by_xpath('//div[@data-load-more-section-id="reviews"]').get_attribute("innerHTML")
-    # soup_expatistan = BeautifulSoup(reviews_div, "html.parser")
-    #
-    # expand_pages = soup_expatistan.find_all("div", class_="single-review")
-    #
-    # for expand_page in expand_pages:
-    #     print("Author Name: ", str(expand_page.find("span", class_="author-name").string.encode("utf-8")))
-    #     print("Review Date: ", expand_page.find("span", class_="review-date").string.encode("utf-8"))
-    #     print("Reviewer Link: ", expand_page.find("a", class_="reviews-permalink")['href'])
-    #     reviewer_ratings = expand_page.find("div", class_="review-info-star-rating").find_next()['aria-label'];
-    #     reviewer_ratings = ''.join(x for x in reviewer_ratings if x.isdigit())
-    #     print("Reviewer Ratings: ", reviewer_ratings)
-    #     print("Review Title: ", str(expand_page.find("span", class_="review-title").string))
-    #     print("Review Body: ", str(expand_page.find("div", class_="review-body").text.encode("utf-8")))
-    #     developer_reply = expand_page.find_parent().find("div", class_="developer-reply")
-    #     if hasattr(developer_reply, "text"):
-    #         print("Developer Reply: ", str(developer_reply.text.encode("utf-8")))
-    #     else:
-    #         print("Developer Reply: ", "")
-
-driver.quit()
+        date = ele.find("span", class_="p2TkOb")
+        reviews = {
+            "user_name": user_name.text,
+            "star_num": star_num,
+            "review": review,
+            "date": date.text
+        }
+        result[index]['reviews'].append(reviews)
+    driver.quit()
+print(result)
