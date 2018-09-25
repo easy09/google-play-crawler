f5e024c50ffed061ade8e2647c1c602ea43f78dc
will <will@cn09.com>
2018-09-25 13:16:12 +0800

    接口完成

--- crawl_play_store.py
+++ crawl_play_store.py
@@ -4,34 +4,31 @@ import sys, io
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.proxy import *
-no_of_reviews = 1000
+from bottle import route, run
 
-non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
-# driver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
-# driver = webdriver.Firefox(executable_path='d:/tools/geckodriver.exe')
-# driver = webdriver.Firefox(executable_path='c:/soft/geckodriver.exe')
 
-# dcap = dict(DesiredCapabilities.PHANTOMJS)
-# dcap["phantomjs.page.settings.userAgent"] = (
-#     "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0")
-# driver = webdriver.PhantomJS("d:/tools/phantomjs.exe", desired_capabilities=dcap)
+@route('/')
+def hello():
+    return {'format': '/app/app_name/token', 'example': '/app/com.twitter.android/123456789'}
 
-options = webdriver.FirefoxOptions()
-options.add_argument('-headless')
-driver = webdriver.Firefox(options=options)
 
-wait = WebDriverWait(driver, 10)
+@route('/app/<name>/<token>')
+def index(name, token):
+    if token != "20180925":
+        return {'message': 'token error'}
+    no_of_reviews = 1000
 
-# Append your app store urls here
-urls = [
-    # "https://play.google.com/store/apps/details?id=com.flipkart.android&hl=en&showAllReviews=true",
-    "https://play.google.com/store/apps/details?id=com.twitter.android&showAllReviews=true"
-    # "https://play.google.com/store/apps/details?id=com.amazon.mShop.android.shopping"
-    # "http://m.toutiao.com"
-]
+    non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
 
-result = []
-for url in urls:
+    options = webdriver.FirefoxOptions()
+    options.add_argument('-headless')
+    # driver = webdriver.Firefox(executable_path="d:/tools/geckodriver.exe", options=options)
+    driver = webdriver.Firefox(executable_path="./geckodriver", options=options)
+
+    wait = WebDriverWait(driver, 10)
+    if not name:
+        return {'param example': "com.twitter.android"}
+    url = "https://play.google.com/store/apps/details?id=" + name + "&showAllReviews=true"
     driver.get(url)
     time.sleep(2)
     driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
@@ -49,17 +46,10 @@ for url in urls:
     page = driver.page_source
 
     soup_expatistan = BeautifulSoup(page, "html.parser")
-    # expand_div = soup_expatistan.find("div", class_="feed-infinite-wrapper")
-    # expand_li = expand_div.find_all("li")
-    # for li in expand_li:
-    #     print(li.text)
     expatistan_table = soup_expatistan.find("h1", class_='AHFaub')
-    result.append({"title": expatistan_table.text, "reviews": []})
-
+    result = {"title": expatistan_table.text, "reviews": []}
     expatistan_table = soup_expatistan.find("div", class_='W4P4ne')
     expatistan_div = expatistan_table.find_all("div", class_='zc7KVe')
-
-    index = len(result) - 1
 
     for ele in expatistan_div:
         user_name = ele.find("span", class_="X43Kjb")
@@ -71,7 +61,6 @@ for url in urls:
             review = review_div
         else:
             review = review_div.find_all("span")[0].text
-
         date = ele.find("span", class_="p2TkOb")
         reviews = {
             "user_name": user_name.text,
@@ -79,6 +68,9 @@ for url in urls:
             "review": review,
             "date": date.text
         }
-        result[index]['reviews'].append(reviews)
+        result['reviews'].append(reviews)
     driver.quit()
-print(result)
+    return dict(result)
+
+
+run(host='0.0.0.0', port=8080)
